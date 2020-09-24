0b08969ca58bc3f87041747749a55b56a62349ab
Vitor Claudio Chaves de Aguiar <vitor.claudio.aguiar@gmail.com>
2020-09-24 20:34:47 -0300

    Saving chat messages on localStorage

--- src/components/pages/ChatContent.tsx
+++ src/components/pages/ChatContent.tsx
@@ -6,23 +6,57 @@ import './ChatContent.css';
 const { TextArea } = Input;
 const { Content } = Layout;
 
-export interface IMessage {
-  value: string;
+export interface ILocalStorageMessage {
   friendId: number;
+  messages: string[];
 }
 
 export default function ChatContent(props: any) {
   const { friend } = props;
-  const [messages, setMessages] = useState([] as IMessage[]);
-  const [message, setMessage] = useState({} as IMessage);
+  const [messages, setMessages] = useState([] as string[]);
+  const [message, setMessage] = useState('' as string);
+
 
   const addMessage = () => {
-    setMessages([message, ...messages]);
-    setMessage({} as IMessage);
+    const mergedMessages = [message, ...messages];
+    const messagesWithFriend: ILocalStorageMessage = {
+      friendId: friend.id,
+      messages: mergedMessages
+    }
+    let messagesPersisted: string | null = localStorage.getItem('messages');
+    if (messagesPersisted) {
+      const parsedMessagesPersisted: ILocalStorageMessage[] | undefined = JSON.parse(messagesPersisted);
+      if (parsedMessagesPersisted) {
+        const index: number = parsedMessagesPersisted.findIndex((item: ILocalStorageMessage) => item.friendId === friend.id);
+        if (index !== -1) {
+          parsedMessagesPersisted[index].messages = mergedMessages;
+          localStorage.setItem('messages', JSON.stringify(parsedMessagesPersisted));
+        } else {
+          parsedMessagesPersisted.push(messagesWithFriend);
+          localStorage.setItem('messages', JSON.stringify(parsedMessagesPersisted));
+        }
+      }
+    } else {
+      localStorage.setItem('messages', JSON.stringify([messagesWithFriend]));
+    }
+    setMessages(mergedMessages);
+    setMessage('' as string);
   }
 
   useEffect(() => {
-    setMessages([] as IMessage[]);
+    const getMessages = () => {
+      let chatMessages = [];
+      let messagesPersisted = localStorage.getItem('messages');
+      if (messagesPersisted) {
+        const parsedMessagesPersisted = JSON.parse(messagesPersisted);
+        if (parsedMessagesPersisted) {
+          const result = parsedMessagesPersisted.find((message: any) => message.friendId === friend.id);
+          chatMessages = result ? result.messages : [];
+        }
+      }
+      return chatMessages;
+    }
+    setMessages(getMessages());
   }, [friend])
 
   const statusStyle = friend.status === 'Online' ? { color: 'green' } : { color: 'red' };
@@ -43,10 +77,10 @@ export default function ChatContent(props: any) {
               }
             </div>
             <div className="chat-content-window">
-            { messages.map((message) => <div className="chat-content-message"> {message.value} </div> )}
+            { messages.map((message) => <div className="chat-content-message"> {message} </div> )}
             </div>
             <div className="chat-content-write">
-              <TextArea rows={4} autoSize={{maxRows: 3, minRows: 3}} value={message.value} onChange={e => setMessage({value: e.target.value, friendId: friend.id})}/>
+              <TextArea rows={4} autoSize={{maxRows: 3, minRows: 3}} value={message} onChange={e => setMessage( e.target.value)}/>
               <Button type="primary" style={{marginLeft: '8px'}} onClick={addMessage}>
                 Submit
               </Button>
