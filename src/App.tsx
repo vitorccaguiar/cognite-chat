600ebb13793d903d312e891bb8a69284876739d0
Vitor Claudio Chaves de Aguiar <vitor.claudio.aguiar@gmail.com>
2020-09-24 13:24:03 -0300

    Add router and login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import './App.css';
+import Login from './components/pages/Login';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
