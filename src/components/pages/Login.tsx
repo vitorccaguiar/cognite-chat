ea81ee0232235d0a1202e217379a8330368d0dfd
Vitor Claudio Chaves de Aguiar <vitor.claudio.aguiar@gmail.com>
2020-09-24 13:28:24 -0300

    Add form login into login component

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
+import { Form, Input, Button, Checkbox, Card } from 'antd';
+import { Link } from 'react-router-dom';
 
 import './Login.css';
 
 export default function Login() {
+  const layout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 16 },
+  };
+  const tailLayout = {
+    wrapperCol: { offset: 10, span: 16 },
+  };
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
   return (
     <div className="login-container">
-      Login
+    <Card title="Login" bordered={true} style={{ width: 500 }}>
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Link to="/menu">
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Link>
+        </Form.Item>
+      </Form>
+    </Card>
     </div>
   )
 }
