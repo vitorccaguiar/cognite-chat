1b7cf469e7779678bf070f9e00e5400014e874dc
Vitor Claudio Chaves de Aguiar <vitor.claudio.aguiar@gmail.com>
2020-09-24 19:04:39 -0300

    Add/load friends behavior for friends list component using localStorage

--- src/components/pages/FriendsList.tsx
+++ src/components/pages/FriendsList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Layout, Menu, Button, Divider } from 'antd';
+import React, { useState } from 'react';
+import { Layout, Menu, Button, Divider, Modal, Input } from 'antd';
 import { UserAddOutlined } from '@ant-design/icons';
 
 import './FriendsList.css';
@@ -13,50 +13,78 @@ export interface IFriendsData {
   status: string;
 }
 
+let idCounter: number = 1;
+
 export default function FriendsList(props: any) {
-  const friendsList: IFriendsData[] = [
-    {
-        id: 1,
-        name: 'Vitor',
-        status: 'Online',
-    },
-    {
-        id: 2,
-        name: 'Paulo',
-        status: 'Online',
-    },
-    {
-        id: 3,
-        name: 'Charles',
-        status: 'Offline',
+  let friends = localStorage.getItem('friends');
+  const parsedFriends = friends ? JSON.parse(friends) as IFriendsData[] : null;
+  const [visible, setVisible] = useState(false);
+  const [newFriendName, setNewFriendName] = useState('');
+  const [friendsList, setFriendsList] = useState(parsedFriends);
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setVisible(false);
+    const friend: IFriendsData = {
+      id: idCounter++,
+      name: newFriendName,
+      status: 'Online'
     }
-  ]
+    let friends = localStorage.getItem('friends');
+    if (friends) {
+      const parsedFriends = JSON.parse(friends) as IFriendsData[];
+      parsedFriends.push(friend);
+      localStorage.setItem('friends', JSON.stringify(parsedFriends));
+      setFriendsList(parsedFriends);
+    } else {
+      localStorage.setItem('friends', JSON.stringify([friend]));
+      setFriendsList([friend]);
+    }
+    setNewFriendName('');
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
 
   const onSelectFriend = (friend: IFriendsData) => {
     props.selectFriend(friend);
   }
 
   return (
-    <Sider
-    style={{
-        overflow: 'auto',
-        height: '100vh',
-        position: 'fixed',
-        left: 0,
-    }}
-    >
-      <div className="friends-list-sider-container">
-        <h2 className="friends-list-title">Friends List</h2>
-        <Button type="primary" shape="circle" icon={<UserAddOutlined />} style={{marginLeft: '8px', marginBottom: '12px', alignSelf: 'flex-end'}}/>
-      </div>
-      <Menu theme="dark" mode="inline">
+    <div>
+      <Sider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+        }}
+      >
+        <div className="friends-list-sider-container">
+          <h2 className="friends-list-title">Friends List</h2>
+          <Button type="primary" shape="circle" icon={<UserAddOutlined />} className="friends-item-add-button" onClick={showModal} />
+        </div>
+        <Menu theme="dark" mode="inline">
           {
-              friendsList.map((friendItem) => 
-                <FriendsListItem key={friendItem.name} friend={friendItem} selectFriend={onSelectFriend}
-              />)
+            friendsList?.map((friendItem: IFriendsData) =>
+              <FriendsListItem key={friendItem.name} friend={friendItem} selectFriend={onSelectFriend}/>)
           }
-          <Divider />
-      </Menu>
-    </Sider>
+        </Menu>
+        <Divider />
+      </Sider>
+      <Modal
+        title="Add friend"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        Type the name of your friend:
+        <Input value={newFriendName} onChange={e => setNewFriendName(e.target.value)}/>
+      </Modal>
+    </div>
   )
 }
